d80bfbda14fa8135967befb0189b5617a57c9547
BigKhay Osobu <samcartexpert@gmail.com>
2024-10-01 13:39:08 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Root route
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, world!");
+  res.send("Hello, KhAY!!!!!");
 });
 
 // A sample API route
